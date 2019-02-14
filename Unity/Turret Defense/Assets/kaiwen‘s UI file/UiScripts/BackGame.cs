e3d92735c11117e3444c1b573e00fe4b3446e33f
kater <maplekater@hotmail.com>
2019-02-14 09:43:39 -0500

    Ui
    UI system

---
+++ Unity/Turret Defense/Assets/kaiwen‘s UI file/UiScripts/BackGame.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class BackGame : MonoBehaviour {
+
+    [SerializeField] public GameObject PausePanel;
+    
+    public bool pausePanel = false;
+
+
+
+    // Use this for initialization
+    void Start()
+    {
+        PausePanel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       // Debug.Log("enter");
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausePanel = true;
+            PausePanel.SetActive(true);
+        }
+        //GamePanelFunction();
+    }
+    public void GamePanelFunction()
+    {
+        Debug.Log("enter");
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            pausePanel = true;
+            PausePanel.SetActive(true);
+        }
+        else if (pausePanel  && Input.GetButtonDown("Jump"))
+        {
+            pausePanel = false;
+            PausePanel.SetActive(false);
+
+        }
+    }
+   
+
+    public void GoBackGame()
+    {
+        pausePanel = false;
+        PausePanel.SetActive(false);
+    }
+}
