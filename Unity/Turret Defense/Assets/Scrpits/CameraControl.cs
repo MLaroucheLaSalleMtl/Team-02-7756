775397177fc34e50f18893d745ebdf67a74158ef
Sutton <suttonzq@gmail.com>
2019-02-16 14:07:31 -0500

    Camera
    Camera updated + need to be fixed

---
+++ Unity/Turret Defense/Assets/Scrpits/CameraControl.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraControl : MonoBehaviour
+{
+
+
+
+
+    void Update()
+    {
+
+        if (Input.mousePosition.y >= Screen.height - 5f)
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime * 10f, Space.World);
+        }
+    
+        if(Input.mousePosition.y <= 5f)
+        {
+            transform.Translate(Vector3.back * Time.deltaTime * 10f,Space.World);
+        }
+        if(Input.mousePosition.x >= Screen.width - 5f)
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * 10f);
+        }
+        if (Input.mousePosition.x <= 5f)
+        {
+            transform.Translate(Vector3.left * Time.deltaTime * 10f);
+        }
+
+        Mathf.Clamp(transform.position.y, 10f, 80f);
+        Mathf.Clamp(transform.position.x, 10f,80f);
+        
+    }
+}
