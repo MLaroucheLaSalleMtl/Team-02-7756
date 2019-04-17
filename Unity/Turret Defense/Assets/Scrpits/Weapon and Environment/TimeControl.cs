913c427f903cd8516f4eba30eadd9ced264c188f
kater <maplekater@hotmail.com>
2019-04-17 17:20:21 -0400

    TimeControl updated

---
+++ Unity/Turret Defense/Assets/Scrpits/Weapon and Environment/TimeControl.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeControl : MonoBehaviour
+{
+    public GameObject TimeButton;
+    public GameObject RewindButton;
+    
+    private bool Speed = false;
+
+    public void PressSpeedin()
+    {
+        Speed = true;
+        Time.timeScale = 1.5f;
+        Debug.Log("Speed Up!");
+        TimeButton.SetActive(false);
+        RewindButton.SetActive(true);
+    }
+
+    
+    public void RewindSpeed()
+    {
+        TimeButton.SetActive(true);
+        RewindButton.SetActive(false);
+        Speed = false;
+        Time.timeScale = 1f;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        TimeButton.SetActive(true);
+        RewindButton.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      
+        
+    }
+}
